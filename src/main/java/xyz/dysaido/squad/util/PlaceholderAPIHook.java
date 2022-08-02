d7a198cedd0b91cac17bce9878fcc5e34835a3a8
Dysaido <tonyyoni@gmail.com>
2022-08-02 17:51:09 +0200

    etc....

--- src/main/java/xyz/dysaido/squad/util/PlaceholderAPIHook.java
+++ src/main/java/xyz/dysaido/squad/util/PlaceholderAPIHook.java
@@ -1,4 +1,51 @@
 package xyz.dysaido.squad.util;
 
-public class PlaceholderApiHook {
+import me.clip.placeholderapi.expansion.PlaceholderExpansion;
+import org.bukkit.entity.Player;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+import xyz.dysaido.squad.SimpleSquad;
+
+public class PlaceholderAPIHook extends PlaceholderExpansion {
+
+    private final SimpleSquad plugin;
+    public PlaceholderAPIHook(SimpleSquad plugin) {
+        this.plugin = plugin;
+    }
+    @Override
+    public @NotNull String getIdentifier() {
+        return plugin.getDescription().getName();
+    }
+
+    @Override
+    public @NotNull String getAuthor() {
+        return plugin.getDescription().getAuthors().toString();
+    }
+
+    @Override
+    public @NotNull String getVersion() {
+        return plugin.getDescription().getVersion();
+    }
+
+    @Override
+    public boolean canRegister() {
+        return true;
+    }
+
+    @Override
+    public boolean persist() {
+        return true;
+    }
+
+    @Override
+    public @Nullable String onPlaceholderRequest(Player player, @NotNull String params) {
+        if (player == null) {
+            return "";
+        } else {
+            if (params.equals("simplesquad")) {
+                return "TRUE";
+            }
+            return "false";
+        }
+    }
 }
