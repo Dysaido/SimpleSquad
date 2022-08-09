c2965097f27f371d7997ff9765ee1db52f1bad9b
Dysaido <tonyyoni@gmail.com>
2022-08-09 18:37:57 +0200

    Add FilterHelper class

---
+++ src/main/java/xyz/dysaido/squad/util/FilterHelper.java
@@ -0,0 +1,18 @@
+package xyz.dysaido.squad.util;
+
+import java.util.Map;
+import java.util.Optional;
+import java.util.stream.Stream;
+
+public class FilterHelper {
+
+    public static  <K, V> Optional<K> getKey(Map<K, V> map, V value) {
+        return map.entrySet().stream().filter(entry -> entry.getValue().equals(value))
+                .map(Map.Entry::getKey).findFirst();
+    }
+
+    public static  <K, V> Stream<K> getKeys(Map<K, V> map, V value) {
+        return map.entrySet().stream().filter(kvEntry -> kvEntry.getValue().equals(value))
+                .map(Map.Entry::getKey);
+    }
+}
