6a8d1fb7dab9461b2573c8b51d1c6831d46bfdbc
Dysaido <tonyyoni@gmail.com>
2022-08-12 19:35:41 +0200

    Add setter to value

--- src/main/java/xyz/dysaido/squad/util/Pair.java
+++ src/main/java/xyz/dysaido/squad/util/Pair.java
@@ -3,7 +3,7 @@ package xyz.dysaido.squad.util;
 public class Pair<K, V> {
 
     private final K key;
-    private final V value;
+    private V value;
 
     public Pair(K key, V value) {
         this.key = key;
@@ -12,6 +12,10 @@ public class Pair<K, V> {
 
     public K getKey() {
         return key;
+    }
+
+    public void setValue(V value) {
+        this.value = value;
     }
 
     public V getValue() {
